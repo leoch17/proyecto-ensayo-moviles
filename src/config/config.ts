a3d10b8d9fb2c101207b95a844df0d1a9324f310
Leonardo Chourio <leoch1000@gmail.com>
2021-06-03 23:08:12 -0400

    init

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,6 @@ export default{
     DB: {
         URI: process.env.MONGODB_URI || 'mongodb://localhost/moviles',
         USER: process.env.MONGODB_USER,
-        PASSWORD: process.env.MONGODB_PASSWORD
+        PASSWORD: process.env.MONGODB_PASSWORD,
     }
 }
